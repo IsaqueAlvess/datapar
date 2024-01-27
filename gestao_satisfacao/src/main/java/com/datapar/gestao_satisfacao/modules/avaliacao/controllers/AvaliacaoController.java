b5edc44cafc62e2b11ceecae542ee5c6ca13fad5
Isaque_Alvess <ysak.alves@outlook.com>
2024-01-27 15:35:09 -0300

    Retornando Lista de Avaliações em formato JSON

--- gestao_satisfacao/src/main/java/com/datapar/gestao_satisfacao/modules/avaliacao/controllers/AvaliacaoController.java
+++ gestao_satisfacao/src/main/java/com/datapar/gestao_satisfacao/modules/avaliacao/controllers/AvaliacaoController.java
@@ -1,14 +1,20 @@
 package com.datapar.gestao_satisfacao.modules.avaliacao.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.datapar.gestao_satisfacao.modules.avaliacao.entities.AvaliacaoEntity;
+import com.datapar.gestao_satisfacao.modules.avaliacao.repositories.AvaliacaoRepository;
 import com.datapar.gestao_satisfacao.modules.avaliacao.services.CreateAvaliacao;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.validation.Valid;
 
@@ -21,7 +27,9 @@ public class AvaliacaoController {
 
     @Autowired
     private CreateAvaliacao createAvaliacao;
-    
+
+    @Autowired
+    private AvaliacaoRepository avaliacaoRepository;
 
     @PostMapping("/")
     public ResponseEntity<Object> createAvaliacao(@Valid @RequestBody AvaliacaoEntity avaliacaoEntity){
@@ -35,5 +43,17 @@ public class AvaliacaoController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }        
     } 
+
+    @GetMapping("/avaliacoes")
+    public ResponseEntity<String> listarAvaliacoes() throws JsonProcessingException{
+        
+        List<AvaliacaoEntity> avaliacoes = avaliacaoRepository.findAll();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(avaliacoes);
+
+        return ResponseEntity.ok(json);
+
+    }
+
 
 }
