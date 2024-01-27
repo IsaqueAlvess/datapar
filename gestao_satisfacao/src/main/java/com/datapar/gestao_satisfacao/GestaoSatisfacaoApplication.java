1fc48e1d60c0df9b4a7b00be37a51a2dae0b24ff
Isaque_Alvess <ysak.alves@outlook.com>
2024-01-27 11:50:15 -0300

    Iniciando projeto
    [CONFIG] Arquivos de configuração e inicialização de projeto

---
+++ gestao_satisfacao/src/main/java/com/datapar/gestao_satisfacao/GestaoSatisfacaoApplication.java
@@ -0,0 +1,13 @@
+package com.datapar.gestao_satisfacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoSatisfacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoSatisfacaoApplication.class, args);
+	}
+
+}
---
+++ gestao_satisfacao/src/test/java/com/datapar/gestao_satisfacao/GestaoSatisfacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datapar.gestao_satisfacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoSatisfacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
